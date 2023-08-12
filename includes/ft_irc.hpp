db2aa898dc7c9fe6f998824ea9f2d2e5faafa42e
etachott <bfwaend@gmail.com>
2023-08-12 14:12:03 -0300

    chore: add headers for used functions

--- includes/ft_irc.hpp
+++ includes/ft_irc.hpp
@@ -3,25 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_irc.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: guribeir <guribeir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: etachott < etachott@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 16:20:48 by etachott          #+#    #+#             */
-/*   Updated: 2023/08/08 19:37:42 by guribeir         ###   ########.fr       */
+/*   Updated: 2023/08/11 17:23:39 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_IRC_HPP
 #define FT_IRC_HPP
 
+#define LOG(x) std::cout << x << std::endl;
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <poll.h>
 
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <cstring>
+#include <algorithm>
+
+#define CLIENT_LIMIT 3
+#define TIMEOUT 1000
 
 typedef struct sockaddr_in SockaddrIn;
 
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -34,54 +34,88 @@ int main(int argc, char **argv) {
 	if (argc < 3)
 		exitError("No port or password!");
 	
-	int			port, n;
+	int			port,
+				opt = 1;
 	socklen_t	clientLen;
-	char		buffer[BUFFER_SIZE];
 	SockaddrIn	servAddr, clientAddr;
 		
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 		exitError("Can't open new socket!");
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT,
+               &opt, sizeof(opt)) < 0)
+		exitError("Failed to set SO_REUSEPORT.");
 	std::memset((char *)&servAddr, 0, sizeof(servAddr));
 	port = std::atoi(argv[1]); // Do port validation ;-;
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_addr.s_addr = INADDR_ANY;
 	servAddr.sin_port = htons(port);
-	if (bind(sockfd, (struct sockaddr *)&servAddr,
-		sizeof(servAddr)) < 0)
+	if (bind(sockfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
 		exitError("Binding failed!");
 	listen(sockfd, 5); // 5 is the max size of the backlog queue for listen
 	clientLen = sizeof(clientAddr);
-	while (true) {
-		newsockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientLen);
-		if (newsockfd < 0)
-			exitError("Accept failed!");
-		std::cout << "Server got connection from "
-			<< inet_ntoa(clientAddr.sin_addr)
-			<< " port " << ntohs(clientAddr.sin_port)
-			<< std::endl;
-		std::string serverName = "TROLL IRC";
-		std::string welcomeMsg = ":myserver 001 yournick :Welcome to the IRC server!\r\n";
-		std::string yourHostMsg = ":myserver 002 yournick :Your host is " + serverName + "\r\n";
-		std::string createdMsg = ":myserver 003 yournick :This server was created sometime\r\n";
-		std::string myInfoMsg = ":myserver 004 yournick " + serverName + " ircd-0.1 aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789\r\n";
-		std::string helloos = "Hello, World!\r\n";
-
-		send(newsockfd, welcomeMsg.c_str(), welcomeMsg.length(), 0);
-		send(newsockfd, yourHostMsg.c_str(), yourHostMsg.length(), 0);
-		send(newsockfd, createdMsg.c_str(), createdMsg.length(), 0);
-		send(newsockfd, myInfoMsg.c_str(), myInfoMsg.length(), 0);
-		send(newsockfd, helloos.c_str(), helloos.length(), 0);
-		std::string hello = std::string(":ft_irc 001 GOD :Hello, world!\r\n");
-		send(newsockfd, hello.c_str(), hello.length(), 0);
-		std::memset(buffer, 0, BUFFER_SIZE);
-		
-		char rcv[1024];
-		int j = recv(newsockfd, rcv, 1024, MSG_DONTWAIT);
-		n = read(newsockfd, buffer, BUFFER_SIZE - 1); // Use poll (or similiar) instead of read
-		if (n < 0)
-			exitError("Reading from socket failed!");
-		std::cout << "The message is: " << buffer << std::endl;
+	struct pollfd fds[CLIENT_LIMIT]; // Define CLIENT_LIMIT as the maximum number of connections you want to support
+	for (int i = 0; i < CLIENT_LIMIT; i++) {
+		fds[i].fd = -1; // Initialize all file descriptors to -1
+		fds[i].events = POLLIN; // We're interested in read events
 	}
+	fds[0].fd = sockfd; // First file descriptor is the listening socket
+	std::cout << "FT_IRC v0.1" << std::endl;
+	while (true) {
+		int activity = poll(fds, CLIENT_LIMIT, TIMEOUT); // TIMEOUT in milliseconds or -1 to wait indefinitely
+
+		if (activity < 0) {
+			std::cerr << "Error: Poll error\n";
+			continue;
+		}
+
+		if (fds[0].revents & POLLIN) {
+			// New connection on the listening socket
+			newsockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientLen);
+			if (newsockfd < 0) {
+				std::cerr << "Error: Accept failed\n";
+				continue;
+			}
+
+			// Add newsockfd to the fds array
+			for (int i = 1; i < CLIENT_LIMIT; i++) {
+				if (fds[i].fd == -1) {
+					fds[i].fd = newsockfd;
+					break;
+				}
+			}
+		}
+
+		// Check for events on client sockets
+		for (int i = 1; i < CLIENT_LIMIT; i++) {
+			if (fds[i].fd != -1 && (fds[i].revents & POLLIN)) {
+				char *bfr = new char[BUFFER_SIZE];
+				std::memset(bfr, 0, BUFFER_SIZE);
+				read(fds[i].fd, bfr, BUFFER_SIZE);
+				std::string msg(bfr);
+				delete[] bfr;
+				/* std::cout << msg; */
+				if (msg.find("USER") != std::string::npos) {
+					std::cout << "Client connected!" << std::endl;
+					std::string response1(":127.0.0.1 001 edu :THIS IS FT_IRC edu!edu@127.0.0.1\r\n"); // Check for repeated names
+					send(fds[i].fd, response1.c_str(), response1.length(), 0);
+				}
+				if (std::strncmp(msg.c_str(), "JOIN", 4) == 0) {
+					std::string response1("edu JOIN :fuba\r\n");
+					std::string response2("ircserv #fuba :FT edu #fuba :FT\r\n");
+					std::string response3("ircserv reply edu = #fuba : edua eduar\r\n:ircserv asdf edu #fuba :fodase\r\n");
+					send(fds[i].fd, response1.c_str(), response1.length(), 0);
+					send(fds[i].fd, response2.c_str(), response2.length(), 0);
+					send(fds[i].fd, response3.c_str(), response3.length(), 0);
+				}
+
+				// If the client closed the connection, close the socket and set fd to -1
+				if (msg.find("QUIT") != std::string::npos) {
+					std::cout << "Client disconnected!" << std::endl;
+					close(newsockfd);
+				}
+			}
+		}
+    }
 	return (0);
 }
