9b9d3a94857ea36a02396625fc33d3b485c3c9ce
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-08-23 18:54:49 -0300

    refactor(registerClient): read broken messages

--- srcs/registerClient.cpp
+++ srcs/registerClient.cpp
@@ -5,9 +5,13 @@ bool registerClient(Client &client)
 	char	buff[BUFFER_SIZE];
 	
 	std::memset(buff, 0, BUFFER_SIZE);
-	if (recv(client.getFd(), buff, BUFFER_SIZE, 0) < 0)
-		throw std::runtime_error("Failed to receive message");
-	std::string content(buff);
+	std::string content;
+	while (count(content.begin(), content.end(), '\n') < 4)
+	{
+		if (recv(client.getFd(), buff, BUFFER_SIZE, 0) < 0)
+			throw std::runtime_error("Failed to receive message");
+		content += buff;
+	}
 	//parse content
 	std::istringstream ss(content);
 	std::string line;
@@ -20,13 +24,13 @@ bool registerClient(Client &client)
 			if (token == "NICK")
 			{
 				ss2 >> token;
-				ss2 >> token;
+				//ss2 >> token;
 				client.setNick(token);
 			}
 			if (token == "USER")
 			{
 				ss2 >> token;
-				ss2 >> token;
+				//ss2 >> token;
 				client.setName(token);
 			}
 			if (token == "PASS")
@@ -34,7 +38,8 @@ bool registerClient(Client &client)
 				ss2 >> token;
 				if (token != "123")
 				{
-					close(client.getFd());
+					close(client.getFd()); //comentei pra tentar fechar esse fd lá fora;
+					Client::decrementIdCounter();
 					ERROR("Wrong password");
 					return (false);
 				}
