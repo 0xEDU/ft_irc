bf615a9eb713f3bdbd0c6d2aaf8911d758cfcd19
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-08-28 12:23:16 -0300

    feat(tests): add minunit setup

--- tests/main_tests.cpp
+++ tests/main_tests.cpp
@@ -1,19 +1,63 @@
 #include "ft_irc_tests.hpp"
+#include "minunit.h"
 
-void assert(bool result, bool expected, const char *test) {
-	if (result == expected) {
-		std::cout << "[" << GREEN << "OK" << END << "]"
-		<< " " << test << std::endl;
-	} else {
-		std::cout << "[" << RED << "KO" << END << "]"
-		<< " " << test << std::endl;
-		std::exit(-1);
+Client *newClient(void) {
+	int port = 2000;
+	pollfd fakePoll[CLIENT_LIMIT];
+
+	int fakeServer = setupTCP(port);
+	fakePoll[0].fd = fakeServer;
+	std::memset(fakePoll, 0, sizeof(pollfd) * CLIENT_LIMIT);
+	for (int i = 0; i < CLIENT_LIMIT; i++)
+	{
+		fakePoll[i].fd = -1;
+		fakePoll[i].events = POLLIN;
 	}
-	return ;
+	fakePoll[0].fd = fakeServer;
+	std::cout << GREEN << "Waiting for client connection" << END << std::endl;
+	while (true) {
+		poll(fakePoll, CLIENT_LIMIT, TIMEOUT);
+		if (fakePoll[0].revents & POLLIN) {
+			Client *client = new Client(fakeServer, fakePoll);
+			return client;
+		}
+	}
+}
+
+void clientInitialization_test(Client *client) {
+	mu_assert(client->getFd() == 4, "FD is correct");
+	mu_assert(client->getId() == 1, "Id is correct");
+}
+
+MU_TEST(clientTests(void) {
+	Client *client = newClient();
+	clientInitialization_test(client);
+	close(client->getFd());
+	delete client;
+}
+
+MU_TEST(parseMsg) {
+	Message response;
+	std::string request;
+	{
+		std::vector<std::string> expectedArgs;
+		expectedArgs.push_back("#channel");
+		expectedArgs.push_back("Hi!");
+
+		request = ":test!~test@test.com PRIVMSG #channel :Hi!";
+		response = parseMsg(request);
+		mu_check(response.prefix == std::string("test!~test@test.com"));
+		mu_check(response.command == std::string("PRIVMSG"));
+		mu_check(response.args == expectedArgs);
+	}
+}
+
+MU_TEST_SUITE(test_suite) {
+	MU_RUN_TEST(parseMsg);
 }
 
 int main(void) {
-	/* clientTests(); */
-	parseMsg_tests();
-	return (0);
+	MU_RUN_SUITE(test_suite);
+	MU_REPORT();
+	return MU_EXIT_CODE;
 }
--- tests/parseMsg_tests.cpp
+++ tests/parseMsg_tests.cpp
@@ -1,15 +1,1 @@
 #include "ft_irc_tests.hpp"
-
-void parseMsg_tests() {
-	std::vector<std::string> response;
-	std::string request;
-	{
-		std::vector<std::string> expected;
-		expected.push_back("test!~test@test.com");
-		expected.push_back("PRIVMSG #channel :Hi!");
-
-		request = ":test!~test@test.com PRIVMSG #channel :Hi!";
-		response = parseMsg(request);
-		asserteq(response, expected, "TEST = :test!~test@test.com PRIVMSG #channel :Hi!");
-	}
-}
