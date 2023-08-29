9cb992da1012e3223159a203b827b3e864ebfb1c
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-08-29 14:32:31 -0300

    refactor: rewrite client validation wip

--- srcs/validateClient.cpp
+++ srcs/validateClient.cpp
@@ -2,45 +2,56 @@
 
 bool validateClient(Client &client)
 {
-	std::string msg = receiveMessage(client);
-	if (std::count(msg.begin(), msg.end(), '\n') != 4
-		&& std::count(msg.begin(), msg.end(), '\r') != 4) {
+	std::string data = receiveData(client);
+	if (std::count(data.begin(), data.end(), '\n') != 4
+		&& std::count(data.begin(), data.end(), '\r') != 4) {
 		ERROR("Malformed client message")
 		close(client.getFd());
 		return (false);
 	}
-	std::istringstream ss(msg);
-	std::string line;
-	while (std::getline(ss, line))
-	{
-		std::string token;
-		std::istringstream ss2(line);
-		while (ss2 >> token)
-		{
-			if (token == "NICK")
-			{
-				ss2 >> token;
-				//ss2 >> token;
-				client.setNick(token);
-			}
-			if (token == "USER")
-			{
-				ss2 >> token;
-				//ss2 >> token;
-				client.setName(token);
-			}
-			if (token == "PASS")
-			{
-				ss2 >> token;
-				if (token != "123")
-				{
-					close(client.getFd()); //comentei pra tentar fechar esse fd lá fora;
-					Client::decrementIdCounter();
-					ERROR("Wrong password");
-					return (false);
-				}
-			}
-		}
-	}
+	// while (true) {
+	// 	string data = receiveData();
+	// 	Message m = parseMsg(data) -> can throw an error;
+	//	validate pass
+	//  useinfo()
+	//	set name, set nick
+	// 	if (client.name && client.nick && pass)
+	//		break ;
+	// }
+	
 	return (true);
 }
+
+// std::istringstream ss(data);
+// std::string line;
+// while (std::getline(ss, line))
+// 	{
+// 		std::string token;
+// 		std::istringstream ss2(line);
+// 		while (ss2 >> token)
+// 		{
+// 			if (token == "NICK")
+// 			{
+// 				ss2 >> token;
+// 				//ss2 >> token;
+// 				client.setNick(token);
+// 			}
+// 			if (token == "USER")
+// 			{
+// 				ss2 >> token;
+// 				//ss2 >> token;
+// 				client.setName(token);
+// 			}
+// 			if (token == "PASS")
+// 			{
+// 				ss2 >> token;
+// 				if (token != "123")
+// 				{
+// 					close(client.getFd()); //comentei pra tentar fechar esse fd lá fora;
+// 					Client::decrementIdCounter();
+// 					ERROR("Wrong password");
+// 					return (false);
+// 				}
+// 			}
+// 		}
+// 	}
