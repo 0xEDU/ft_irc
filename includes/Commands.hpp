192b46b1f1806571fda47aff3634bc1ea0f98c81
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-09-04 20:14:10 -0300

    feat(map): make map logic receive pair

--- includes/Commands.hpp
+++ includes/Commands.hpp
@@ -17,12 +17,14 @@ class Commands
 	public:
 
 	typedef void (*funcPtr)(CommandArgs);
-	static void populateMap(void);
-	static void callFunction(const std::string cmdName, CommandArgs args);
+
+	static void 		populateMap(void);
+	static void 		callFunction(const std::string cmdName, CommandArgs args);
+	static std::string	*getResponseMessage(const std::string cmdName);
 
 	private:
 
-	static std::map<std::string, funcPtr> _messageFunctions;
+	static std::map<std::string, std::pair<funcPtr, std::string *> > _messageFunctions;
 };
 
 void nick(CommandArgs cArgs);
--- srcs/Commands.cpp
+++ srcs/Commands.cpp
@@ -1,18 +1,23 @@
 #include "ft_irc.hpp"
 
-std::map<std::string, Commands::funcPtr> Commands::_messageFunctions;
+std::map<std::string, std::pair<Commands::funcPtr, std::string*> > Commands::_messageFunctions;
 
 void Commands::populateMap(void)
 {
-	_messageFunctions["USER"] = user;
-	_messageFunctions["NICK"] = nick;
-	_messageFunctions["PASS"] = pass;
-	_messageFunctions["CAP"] = cap;
+	_messageFunctions["USER"] = std::make_pair<funcPtr, std::string *>(user, NULL);
+	_messageFunctions["NICK"] = std::make_pair<funcPtr, std::string *>(nick, NULL);
+	_messageFunctions["PASS"] = std::make_pair<funcPtr, std::string *>(pass, NULL);
+	_messageFunctions["CAP"] = std::make_pair<funcPtr, std::string *>(cap, NULL);
 }
 
 void Commands::callFunction(const std::string cmdName, CommandArgs args)
 {
 	if (_messageFunctions.find(cmdName) == _messageFunctions.end())
 		throw std::runtime_error("Invalid Command Name");
-	_messageFunctions[cmdName](args);
+	_messageFunctions[cmdName].first(args);
+}
+
+std::string *Commands::getResponseMessage(const std::string cmdName)
+{
+	return (_messageFunctions[cmdName].second);
 }
