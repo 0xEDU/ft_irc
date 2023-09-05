0c2ca74bccdd2c4f6b90597708295aa4ddc16c8d
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-09-05 20:02:43 -0300

    refactor: revert to original map

--- srcs/commands/Commands.cpp
+++ srcs/commands/Commands.cpp
@@ -1,23 +1,18 @@
 #include "ft_irc.hpp"
 
-std::map<std::string, std::pair<Commands::funcPtr, std::string*> > Commands::_messageFunctions;
+std::map<std::string, Commands::funcPtr> Commands::_messageFunctions;
 
 void Commands::populateMap(void)
 {
-	_messageFunctions["USER"] = std::make_pair<funcPtr, std::string *>(user, NULL);
-	_messageFunctions["NICK"] = std::make_pair<funcPtr, std::string *>(nick, NULL);
-	_messageFunctions["PASS"] = std::make_pair<funcPtr, std::string *>(pass, NULL);
-	_messageFunctions["CAP"] = std::make_pair<funcPtr, std::string *>(cap, NULL);
+	_messageFunctions["USER"] = user;
+	_messageFunctions["NICK"] = nick;
+	_messageFunctions["PASS"] = pass;
+	_messageFunctions["CAP"] = cap;
 }
 
-void Commands::callFunction(const std::string cmdName, CommandArgs args)
+std::string Commands::callFunction(const std::string cmdName, CommandArgs args)
 {
 	if (_messageFunctions.find(cmdName) == _messageFunctions.end())
 		throw std::runtime_error("Invalid Command Name");
-	_messageFunctions[cmdName].first(args);
-}
-
-std::string *Commands::getResponseMessage(const std::string cmdName)
-{
-	return (_messageFunctions[cmdName].second);
+	return (_messageFunctions[cmdName](args));
 }
