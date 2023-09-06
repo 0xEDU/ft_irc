4e7cbb491c3dc0d920645d79109f3751d0786026
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-09-06 19:00:27 -0300

    fix(reply): nick error message fixed

--- includes/replies.hpp
+++ includes/replies.hpp
@@ -4,7 +4,7 @@
 
 #define RPL_WELCOME(nick, user) ":* 001 "+ user + " :Welcome to the Internet Relay Chat " + nick +"!" + user + "@*\r\n"
 
-#define ERR_NICKNAMEINUSE(nick) ":* 433 " + nick + " :Nickname alredy in use\r\n"
+#define ERR_NICKNAMEINUSE(nick) ":* 433 * " + nick + " :Nickname asdfasdfasdfa\r\n"
 #define ERR_ALREADYREGISTERED(user) ":* 462 " + user + " :User alredy registered\r\n"
 
 
