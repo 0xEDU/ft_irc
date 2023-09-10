43e36958d9bc84d6ebd3e04dfb0d8e11eac6956e
Guiribei <guilherme_f_ribeiro@hotmail.com>
2023-09-10 17:19:22 -0300

    refactor(NICK): change NICK flow

--- srcs/commands/CAP.cpp
+++ srcs/commands/CAP.cpp
@@ -1,5 +1,7 @@
 #include "ft_irc.hpp"
 
+void printBytes(const std::string& str);
+
 std::string cap(CommandArgs cArgs)
 {
 	(void)cArgs;
--- srcs/commands/NICK.cpp
+++ srcs/commands/NICK.cpp
@@ -3,10 +3,12 @@
 std::string nick(CommandArgs cArgs)
 {
 	std::string nickname = cArgs.msg.args[0];
+	if (nickname == "")
+		return (ERR_ERRONEUSNICKNAME(nickname));
 	for (std::size_t i = 0; i < cArgs.clients.size(); i++)
 	{
 		if (cArgs.clients[i].getNick() == nickname)
-			return (ERR_NICKNAMEINUSE(nickname));
+			return ERR_NICKNAMEINUSE(nickname);
 	}
 	cArgs.client.setNick(nickname);
 	return ("");
