cf6431f6282d2fcaa2d4d166c46ba0cc383bbcbd
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-09-29 17:27:36 -0300

    feat: get rid of while loop on receiveData

--- srcs/receiveData.cpp
+++ srcs/receiveData.cpp
@@ -1,41 +1,17 @@
 #include "ft_irc.hpp"
 
-
 std::string receiveData(Client &client)
 {
 	std::string data;
-	// const int maxRetries = 5; // Maximum retries
 
-	while (true)
-	{
-		char buff[BUFFER_SIZE];
-		std::memset(buff, 0, BUFFER_SIZE);
+	char buff[BUFFER_SIZE];
+	std::memset(buff, 0, BUFFER_SIZE);
 
-		long nbytes = recv(client.getFd(), buff, BUFFER_SIZE, 0);
-		if (nbytes < 0)
-		{
-			if (errno == EAGAIN || errno == EWOULDBLOCK)
-			{
-				if (data.find("\r\n") != std::string::npos)
-					break;
-				continue;
-			}
-			else
-			{
-				std::cerr << "recv() failed with errno: " << errno << std::endl;
-				throw std::runtime_error("Failed to receive message");
-			}
-		}
-		else if (nbytes == 0)
-		{
-            client.setShouldEraseClient(true);
-            break;
-		}
-		else
-		{
-			data.append(buff, nbytes);
-		}
-	}
+	long nbytes = recv(client.getFd(), buff, BUFFER_SIZE, 0);
+	if (nbytes == 0)
+		client.setShouldEraseClient(true);
+	else
+		data.append(buff, nbytes);
     std::cout << "RECEIVED: " << data << std::endl;
 	return data;
 }
