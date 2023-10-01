a10c22da4bde6a90c8efab79c41a46da9bcf1290
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-10-01 14:36:00 -0300

    USER pass fixed

--- srcs/commands/USER.cpp
+++ srcs/commands/USER.cpp
@@ -14,7 +14,10 @@ std::string user(CommandArgs cArgs)
 	if (cArgs.client.getShouldEraseClient())
 		return ("");
 	if (cArgs.client.getPass().empty())
+	{
+		cArgs.client.setShouldEraseClient(true);
 		return("");
+	}
 	cArgs.client.setUser(username);
 	cArgs.client.setRealName(cArgs.msg.args[3]);
 	if (cArgs.client.getRetries())
